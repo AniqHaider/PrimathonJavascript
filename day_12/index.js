15fdcacf3c87052d97c7991a92f27de06aa659a6
Aniq Haider <aniqhaider1@gmail.com>
2023-04-18 18:24:21 +0530

    updates

--- day_12/index.js
+++ day_12/index.js
@@ -1,1 +1,12 @@
 // Exception Handling ( error handling )
+
+// basically we can find out which code block is causing error like this and console log the type and also return the output as error name or message but this lets the rest of the code block execute as it is ...
+
+try{
+    console.log(aniq)
+}catch(e){
+    console.log(e.name)
+    console.log(e.message)
+}
+
+console.log("ANIQ")
